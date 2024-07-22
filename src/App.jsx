884ffac3f469b81e3939405d0cb9a512cda0b914
mr-dev-prince <pkcofficial24@gmail.com>
2024-07-22 12:52:03 +0530

    checkup -3

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
   }, []);
   return (
     <div>
+      <h1 className="text-xl text-black ">Prince Kumar</h1>
       <h1 className="text-xl font-bold text-black">{data?.par}</h1>
     </div>
   );
